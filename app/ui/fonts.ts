3ec945a694716f7e2bbfd79568e42d1b497d7da5
ManasaGangumolu <manasa.gangumolu@data-axle.com>
2023-12-12 14:52:18 +0530

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana} from "next/font/google";
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight:["700"],subsets:['latin']})
+
+// options: { weight: "400" | "700" | ("400" | "700")[]; style?: 
+// "normal" | "normal"[] | undefined; display?: Display | 
+// undefined; variable?: undefined; preload?: boolean | undefined; fallback?: string[] | 
+// undefined; adjustFontFallback?: boolean | undefined; subsets?: "latin"[] | undefined; })
